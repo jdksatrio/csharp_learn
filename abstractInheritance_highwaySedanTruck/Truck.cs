2d4417916eeb478195d7918b1fe5ea01e36cd953
jdksatrio <117372398+jdksatrio@users.noreply.github.com>
2023-01-05 15:12:23 +0700

    Create Truck.cs

---
+++ abstractInheritance_highwaySedanTruck/Truck.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace LearnInheritance
+{
+  class Truck : Vehicle, IAutomobile
+  { 
+    public double Weight
+    { get; }
+
+    public Truck(double speed, double weight) : base(speed)
+    {
+      Weight = weight;
+
+      if (weight < 400)
+      {
+        Wheels = 8;
+      }
+      else
+      {
+        Wheels = 12;
+      }
+    }
+    public override string Describe(){
+      return $"This truck is moving on {Wheels} wheels at {Speed} km/h, with license plate {LicensePlate}";
+    }
+
+  }
+}
