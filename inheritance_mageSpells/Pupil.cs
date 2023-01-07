89019b5ddf000fa005ef2e5383bbd32e0ea44f78
jdksatrio <117372398+jdksatrio@users.noreply.github.com>
2023-01-07 22:47:52 +0700

    Create Pupil.cs

---
+++ inheritance_mageSpells/Pupil.cs
@@ -0,0 +1,18 @@
+// Pupil.cs
+using System;
+
+namespace MagicalInheritance
+{
+  class Pupil
+  {
+    public string Title { get; private set; }
+
+    public Pupil(string title){
+      Title = title;
+    }
+
+    public Storm CastWindStorm(){
+      return new Storm("wind", false, Title);
+    }
+  }
+}
